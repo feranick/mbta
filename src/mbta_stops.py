cac62edd0dda5ccc442d5087d713d76898b0bb34
Nicola Ferralis <feranick@hotmail.com>
2023-12-30 23:41:57 -0500

    Find names and ids of stops

---
+++ src/mbta_stops.py
@@ -0,0 +1,17 @@
+from pymbta3 import Stops, Vehicles
+
+key = "91944a70800a4bcabe1b9c2023d12fc8"
+
+line = "Orange"
+line = "Red"
+line = "Blue"
+
+st = Stops(key=key)
+stops = st.get(route=line)['data']
+stops = st.get(id=station)['data']
+
+for s in stops:
+    print(s)
+    print(s['id'],s['attributes']['name'])
+
+
