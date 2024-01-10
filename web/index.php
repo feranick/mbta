a0cef30dbe2ca331e785964659d18c3b74c9268c
Nicola Ferralis <feranick@hotmail.com>
2024-01-10 18:57:13 -0500

    minor fixes

--- web/index.php
+++ web/index.php
@@ -12,10 +12,6 @@ if ($_POST['station'] and $_POST['lines']) {
     $station = $_POST['station'];
     $command = "./mbta_lines.py $station 2>&1";
     $output = shell_exec($command);
-} else if($_POST['vehicle']) {
-    $vehicle = $_POST['vehicle'];
-    $command = "./mbta_vehicles_id.py $vehicle 2>&1";
-    $output = shell_exec($command);
 }
 ?>
 
@@ -23,23 +19,18 @@ if ($_POST['station'] and $_POST['lines']) {
 <html><head>
 
 <title>MBTA</title>
-
-    		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0" />
-            
-            <!-- Eliminate url and button bars if added to home screen -->
-            <meta name="apple-mobile-web-app-capable" content="yes" />
-            
-            <!-- Choose how to handle the phone status bar -->
-            <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-            
-            <link rel="stylesheet" type="text/css" href="mbta.css" media="screen" />
-            
-            <!-- Choose a 57x57 image for the icon -->
-            <link rel="apple-touch-icon" href="mbta_icon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0" />
+    <!-- Eliminate url and button bars if added to home screen -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <!-- Choose how to handle the phone status bar -->
+    <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <link rel="stylesheet" type="text/css" href="mbta.css" media="screen" />
+    <!-- Choose a 57x57 image for the icon -->
+    <link rel="apple-touch-icon" href="mbta_icon.png" />
 </head>
 <body>
 
-<form name = "MBTA_sign" action = "index.php" method = "POST">
+<form name = "MBTA" action = "index.php" method = "POST">
 <br>Station =  <input name="station" value="<?php echo $station;?>" placeholder="station" size="12" maxlength="30" type="text" id="Entry">
 <br><br>Lines =  <input name="lines" value="<?php echo $lines;?>" placeholder="lines" size="12" maxlength="50" type="text" id="Entry">
 <br><br>Vehicle ID =  <input name="vehicle" value="<?php echo $vehicle;?>" placeholder="vehicle id" size="12" maxlength="50" type="text" id="Entry">
@@ -47,7 +38,4 @@ if ($_POST['station'] and $_POST['lines']) {
 </form>
 
 <text_area><pre><?php echo $output; ?></pre></text_area>
-
-
-
 </body></html>
